64028c5b263ead77fd1b663e7dc0d1cb2b25473e
babatunde <babatunde@techloftgh.com>
2019-08-17 04:08:14 +0000

    project initialization

--- build/frontend_server.d
+++ build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: /Users/root1/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
+frontend_server.d: /Users/gman/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
